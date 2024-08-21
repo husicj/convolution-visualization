25debd00492c5857ad0b9cbe27acc65b1c72334c
Joren Husic <husicjd@gmail.com>
2024-08-21 16:51:25 -0600

    initial commit with project goals

---
+++ visualize_convolution.py
@@ -0,0 +1,16 @@
+# GOALS:
+#   generate visualizations of convolutions between:
+#      rectangle
+#      isoceles triangle
+#      right triangle
+#      causally decreasing exponential function
+#
+#   computationally confirm the following properties of convolution
+#    with RMS error calculation:
+#     commutativity
+#     assiciativity
+#     distributivity
+#   and demonstrating through animation the multiplicative identity
+
+
+
